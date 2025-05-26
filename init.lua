b8ab1cd62acde3c6008e311748ec5903fb3ea510
advokatenbby <oliver.saari_@outlook.com>
2025-05-26 20:15:58 +0200

    Added line numbers

--- init.lua
+++ init.lua
@@ -5,8 +5,10 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+vim.wo.number = true
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
+vim.cmd("colorscheme onedark")
 
