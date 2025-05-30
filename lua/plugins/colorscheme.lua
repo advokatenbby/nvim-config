8df20405bf115500cad68629385968b160b62811
advokatenbby <oliver.saari_@outlook.com>
2025-05-30 14:11:54 +0200

    Added new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,5 +7,13 @@ return {
     {
         "olimorris/onedarkpro.nvim", 
         priority = 1000 
+    }, 
+    {
+        "AlexvZyl/nordic.nvim", 
+        lazy = false, 
+        priority = 1000, 
+        config = function()
+            require("nordic").load()
+        end
     }
 }
